19b839ed45f7a73c263c50613121a61695e3fafe
Jorge Oliveira <jorgedroliveira10@gmail.com>
2021-09-09 16:55:59 +0100

    added random array function

--- sorting-algorithms-visualizer/src/components/Bar/Bar.js
+++ sorting-algorithms-visualizer/src/components/Bar/Bar.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
 
 const Bar = ({ height, color }) => {
   const style = {
-    width: "30px",
+    width: "10px",
     height: height + "px",
     borderRadius: "15px",
     backgroundColor: color,
+    marginRight: "1px",
   };
 
-  return <div style={style} />;
+  return <Col style={style} xs={12} />;
 };
 
 export default Bar;
--- sorting-algorithms-visualizer/src/components/SortingVisualizer/SortingVisualizer.js
+++ sorting-algorithms-visualizer/src/components/SortingVisualizer/SortingVisualizer.js
@@ -1,41 +1,50 @@
 import React, { useState, useRef, useEffect } from "react";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Bar from "../Bar/Bar";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import { bubbleSort } from "../../algorithms/BubbleSort";
 
-const arrayToSort = [6, 3, 1, 7, 6, 5, 4, 3, 2, 1];
+const arrayToSort = randomArray();
 const steps = bubbleSort(arrayToSort);
 const colorSteps = steps[1];
 const sortingSteps = steps[0];
 
+function randomArray() {
+  return [...Array(50)].map((_) => Math.ceil(Math.random() * 50));
+}
+
 const SortingVisualizer = () => {
   const currentIndex = useRef(0);
+  const [render, setRender] = useState(false);
   const [array, setArray] = useState([]);
 
   useEffect(() => {
-    console.log(colorSteps.length)
-    setTimeout(() => {
-      setArray(sortingSteps[currentIndex.current]);
-    }, 1000);
-
-    if (currentIndex.current + 1 < sortingSteps.length) {
-      currentIndex.current++;
-      
+    if (!render && currentIndex.current === 0) {
+      setArray(sortingSteps[0]);
     }
-  }, [array]);
+
+    if (render) {
+      setTimeout(() => {
+        setArray(sortingSteps[currentIndex.current]);
+      }, 1000);
+
+      if (currentIndex.current + 1 < sortingSteps.length) {
+        currentIndex.current++;
+      }
+    }
+  }, [render, array]);
 
   return (
-    <Container>
-      <Row>
+    <Container fluid>
+      <Button onClick={() => setRender(!render)}> Start/Stop</Button>
+      <Row className="justify-content-center">
         {array.map((element, index) => (
-          <Col key={index}>
-            <Bar
-              height={element * 50}
-              color={colorSteps[currentIndex.current][index]}
-            />
-          </Col>
+          <Bar
+            key={index}
+            height={element * 10}
+            color={colorSteps[currentIndex.current][index]}
+          />
         ))}
       </Row>
     </Container>
