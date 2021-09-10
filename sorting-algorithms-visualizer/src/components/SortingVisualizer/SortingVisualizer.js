5806aad76b8c2273f704a10c16c88f6bcae90786
Jorge Oliveira <jorgedroliveira10@gmail.com>
2021-09-10 19:18:36 +0100

    code refactor

--- sorting-algorithms-visualizer/src/components/SortingVisualizer/SortingVisualizer.js
+++ sorting-algorithms-visualizer/src/components/SortingVisualizer/SortingVisualizer.js
@@ -5,50 +5,61 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { bubbleSort } from "../../algorithms/BubbleSort";
 
-const arrayToSort = randomArray();
-const steps = bubbleSortAlgorithm();
-const colorSteps = steps[1];
-const sortingSteps = steps[0];
-
-function randomArray() {
-  return [...Array(50)].map((_) => 50 + Math.ceil(Math.random() * 500));
-}
-
-function bubbleSortAlgorithm() {
-  return bubbleSort(arrayToSort);
-}
-
 const SortingVisualizer = () => {
   const currentIndex = useRef(0);
-  const [render, setRender] = useState(false);
-  const [array, setArray] = useState([]);
+  const [steps, setSteps] = useState({ sortingSteps: [], colorSteps: [] });
 
   useEffect(() => {
-    if (!render && currentIndex.current === 0) {
-      setArray(sortingSteps[0]);
-    }
+    setSteps({
+      sortingSteps: generateRandomArray(),
+      colorSteps: Array(steps.sortingSteps.length).fill("#DCDCDC"),
+    });
+  }, [steps.sortingSteps.length]);
 
-    if (render) {
-      setTimeout(() => {
-        setArray(sortingSteps[currentIndex.current]);
-      }, 500);
+  const generateRandomArray = () => {
+    return [...Array(50)].map((_) => 50 + Math.ceil(Math.random() * 500));
+  };
 
-      if (currentIndex.current + 1 < sortingSteps.length) {
+  const bubbleSortAnimation = async () => {
+    const bubbleSortData = bubbleSort(steps.sortingSteps);
+    const sorting = bubbleSortData[0];
+    const color = bubbleSortData[1];
+
+    setSteps({
+      sortingSteps: sorting[0],
+      colorSteps: color[0],
+    });
+
+    for (let i = 1; i <= sorting.length; i++) {
+      setSteps({
+        sortingSteps: sorting[currentIndex.current],
+        colorSteps: color[currentIndex.current],
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 5));
+
+      if (currentIndex.current + 1 < sorting.length) {
         currentIndex.current++;
       }
     }
-  }, [render, array]);
+  };
 
   return (
     <Container fluid>
-      <Button onClick={() => setRender(!render)}>Start/Stop</Button>
+      <Button
+        onClick={() =>
+          setSteps({
+            sortingSteps: generateRandomArray(),
+            colorSteps: Array(steps.sortingSteps.length).fill("#DCDCDC"),
+          })
+        }
+      >
+        New array
+      </Button>
+      <Button onClick={() => bubbleSortAnimation()}>Start/Stop</Button>
       <Row className="justify-content-center">
-        {array.map((element, index) => (
-          <Bar
-            key={index}
-            height={element}
-            color={colorSteps[currentIndex.current][index]}
-          />
+        {steps.sortingSteps.map((element, index) => (
+          <Bar key={index} height={element} color={steps.colorSteps[index]} />
         ))}
       </Row>
     </Container>
